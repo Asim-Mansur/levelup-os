48ba0ffe690ce4cb2f03c085bbb23d76b533a47f
Asim Mansur <asimmansur.seps@gmail.com>
2026-06-06 13:51:09 +0530

    Fix frontend API URLs

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   formData.append("password", password);
 
   const response = await fetch(
-    "http://127.0.0.1:8000/login",
+    "https://levelup-os-production.up.railway.app",
     {
       method: "POST",
       body: formData,
